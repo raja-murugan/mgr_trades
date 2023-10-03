b56fa8823a3175cebd077f6f708477af8e93e60c
Rajamurugan Thirumaran <muruganraja335@gmail.com>
2023-10-03 13:14:01 +0530

    main

--- resources/views/layout/frontend/components/mobile-navbar.blade.php
+++ resources/views/layout/frontend/components/mobile-navbar.blade.php
@@ -15,19 +15,17 @@
         <ul class="mobile-nav__contact list-unstyled">
             <li>
                 <i class="fa fa-envelope"></i>
-                <a href="mailto:needhelp@packageName__.com">needhelp@endreox.com</a>
+                <a href="mailto:info@mgrtrades.com">info@mgrtrades.com</a>
             </li>
             <li>
                 <i class="fa fa-phone-alt"></i>
-                <a href="tel:666-888-0000">666 888 0000</a>
+                <a href="tel:9994023234">+91 99940 23234</a>
             </li>
         </ul><!-- /.mobile-nav__contact -->
         <div class="mobile-nav__top">
             <div class="mobile-nav__social">
-                <a href="#" class="fab fa-twitter"></a>
-                <a href="#" class="fab fa-facebook-square"></a>
-                <a href="#" class="fab fa-pinterest-p"></a>
-                <a href="#" class="fab fa-instagram"></a>
+                <a href="https://www.facebook.com/mgrtrades2020" target="_blank" class="fab fa-facebook-square"></a>
+                <a href="https://www.instagram.com/mgrtrades2020/" target="_blank" class="fab fa-instagram"></a>
             </div><!-- /.mobile-nav__social -->
         </div><!-- /.mobile-nav__top --
     </div>
